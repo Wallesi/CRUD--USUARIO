b6dd8cd41f7a94c9f8050aaa1e4447dc011d5bfe
Wallesi <waldemargalizzi98@gmail.com>
2022-11-19 17:08:09 -0300

    Login y registro completado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EpleadosDashboardComponent } from './epleados-dashboard/epleados-dashboard.component';
 import { HttpClientModule } from '@angular/common/http';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EpleadosDashboardComponent
+    EpleadosDashboardComponent,
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ReactiveFormsModule,
-    HttpClientModule
+    FormsModule,
+    ReactiveFormsModule, //Necesario para el signup
+    HttpClientModule //Necesario para el signup
   ],
   providers: [],
   bootstrap: [AppComponent]
